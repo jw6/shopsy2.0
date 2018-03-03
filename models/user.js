9ed3c85ed4743212948e08636278eaa801ec8aa0
Jay Wang <jaywang007@yahoo.com>
2018-03-02 21:34:34 -0600

    implemented GET and POST for user

--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const config = require('../config/database');
 
+const SALT_WORK_FACTOR = 10;
+
 // User schema
 const UserSchema = mongoose.Schema({
   name: {
@@ -19,3 +21,34 @@ const UserSchema = mongoose.Schema({
 }, { timestamp: true })
 
 const User = module.exports = mongoose.model('User', UserSchema);
+
+// GET request
+module.exports.getUserById = function(id, callback) {
+  User.findById(id,callback);
+}
+
+// GET request
+module.exports.getUserByEmail = function(email, callback) {
+  const query = { email: email }
+  User.findOne(query, callback);
+}
+
+// GET request
+module.exports.getUserByEmail = function(username, callback) {
+  const query = { username: username }
+  User.findOne(query, callback);
+}
+
+// POST request
+module.exports.addUser = function(newUser, callback) {
+  // Using salt to encrypt password in mongodb
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err,  hash) => {
+      if(err) {
+        throw err;
+      }
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+}
